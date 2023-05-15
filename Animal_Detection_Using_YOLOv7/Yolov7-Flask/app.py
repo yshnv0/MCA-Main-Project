61634dbb3e5d1b56e3248a46fe3ad01a4fd57be1
yshnv <vaishnavp324@gmail.com>
2023-05-15 19:45:36 +0530

    main-project

---
+++ Animal_Detection_Using_YOLOv7/Yolov7-Flask/app.py
@@ -0,0 +1,66 @@
+import io
+from operator import truediv
+import os
+import json
+import PIL
+from PIL import Image
+import argparse
+import torch
+from flask import Flask, jsonify, url_for, render_template, request, redirect
+
+app = Flask(__name__)
+
+RESULT_FOLDER = os.path.join('static')
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+
+# finds the model inside your directory automatically - works only if there is one model
+def find_model():
+    for f  in os.listdir():
+        if f.endswith(".pt"):
+            return f
+    print("please place a model file in this directory!")
+    
+model_name = find_model()
+model =torch.hub.load("WongKinYiu/yolov7", 'custom',model_name)
+
+model.eval()
+
+def get_prediction(img_bytes):
+    img = Image.open(io.BytesIO(img_bytes))
+    imgs = [img]  # batched list of images
+# Inference
+    results = model(imgs, size=640)  # includes NMS
+    return results
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+            
+        img_bytes = file.read()
+        results = get_prediction(img_bytes)
+        results.save(save_dir='static')
+        filename = 'image0.jpg'
+        
+        return render_template('result.html',result_image = filename,model_name = model_name)
+
+    return render_template('index.html')
+@app.route('/detect', methods=['GET', 'POST'])
+def handle_video():
+    # some code to be implemented later
+    pass
+
+@app.route('/webcam', methods=['GET', 'POST'])
+def web_cam():
+    # some code to be implemented later
+    pass
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask app exposing yolov7 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
